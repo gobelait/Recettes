fc1d1f214bda639cecfeb447303e649bdce04bc0
gueyemoo <m.gueye0411@gmail.com>
2021-03-22 17:04:22 +0100

    push titre laravel

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -9,4 +9,13 @@ class RecipesController extends Controller
   function index() {
       return view('recipes');
   }
+
+  public function show($recipe_name) {
+   $recipe = \App\Models\Recipe::where('recipe_name',$recipe_name)->first(); //get first recipe with recipe_nam == $recipe_name
+
+   return view('recipes/single',array( //Pass the recipe to the view
+       'recipe' => $recipe
+   ));
+}
+
 }
