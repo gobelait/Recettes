c813650cd40ba8c802dcadf784a0aa5ba49aaae4
gueyemoo <m.gueye0411@gmail.com>
2021-03-30 12:39:17 +0200

    recitfication page recipe

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -6,7 +6,6 @@
 
       <a href="/"></a>
 
-<<<<<<< HEAD
       <body>
       <div class="container-lg">
           <div class="table-responsive">
@@ -65,7 +64,4 @@
           </div>
       </div>
       </body>
-=======
-      <a class="btn" href="{{ route('recettes.create') }}" title="Ajouter une recette"> <i class="fas fa-plus-circle"></i> Ajout </a>
->>>>>>> 3bd36676d9e511560b551312eba4546b0c1226b6
       @endsection
--- routes/web.php
+++ routes/web.php
@@ -26,29 +26,13 @@ Route::get('/recettes/{url}',[RecipesController::class, 'show']);
 
 //Controlleurs pour le CRUD des recettes
 use App\Http\Controllers\RecettesController;
-<<<<<<< HEAD
 Route::resource('/admin/recettes', RecettesController::class);
 Route::get('/admin/recettes.delete/{url}', [RecettesController::class, 'delete']);
-=======
-Route::get('/admin/recettes', 'RecettesController@index')->name('recettes.index');
-Route::get('/admin/recettes/create', 'RecettesController@create')->name('recettes.create');
-Route::post('/admin/recettes/', 'RecettesController@store')->name('recettes.store');
->>>>>>> 3bd36676d9e511560b551312eba4546b0c1226b6
+
 
 Route::resource('/admin/recettes', RecettesController::class);
 
-<<<<<<< HEAD
-Route::resource('/admin/recettes', RecettesController::class);
-
-=======
->>>>>>> 3bd36676d9e511560b551312eba4546b0c1226b6
 //Controlleurs pour le formulaire de contact
 use App\Http\Controllers\ContactController;
 Route::get('/contact', [ContactController::class, 'index']);
 Route::post('/contact', [ContactController::class, 'store']);
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 3bd36676d9e511560b551312eba4546b0c1226b6
